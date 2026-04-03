19e44f0e3996c29b75f74872c2e6995e824ee18f
Carsten <carstenhanekamp.1@gmail.com>
2026-04-03 16:43:00 +0200

    feat: add @vercel/analytics integration and update package dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import LandingPage from './LandingPage';
 import PaperviewApp from './PaperviewApp';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/app" element={<PaperviewApp />} />
-      <Route path="/app/*" element={<PaperviewApp />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<PaperviewApp />} />
+        <Route path="/app/*" element={<PaperviewApp />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
